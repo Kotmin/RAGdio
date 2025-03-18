04e0d2247276badfd8d8f7ec27a5cfe91d7195a1
Kotmin <70173732+Kotmin@users.noreply.github.com>
2025-03-18 20:15:11 +0100

    feat: change default transcribe model to whisper api

--- RAGdio-backend/app/core/config.py
+++ RAGdio-backend/app/core/config.py
@@ -8,6 +8,6 @@ load_dotenv()
 class Config:
     """App configuration settings."""
 
-    ASR_MODEL = os.getenv("ASR_MODEL", "whisper")  # Default: Whisper
+    ASR_MODEL = os.getenv("ASR_MODEL", "whisper-api")  # Default: Whisper
     DEBUG = os.getenv("DEBUG", "False").lower(
     ) == "true"  # Convert string to bool
