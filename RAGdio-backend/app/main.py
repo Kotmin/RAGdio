c081e93daafe54afe133c00b6c6a7becbe10edd8
Kotmin <70173732+Kotmin@users.noreply.github.com>
2025-03-20 23:25:36 +0100

    fix : add config to main

--- RAGdio-backend/app/main.py
+++ RAGdio-backend/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from app.api.audio import router as audio_router
+from app.core.config import Config
 
 # Initialize FastAPI
-app = FastAPI(title="RAG Over Audio", description="Transcribe audio and use RAG for LLM queries.")
+app = FastAPI(title="RAG Over Audio",
+              description="Transcribe audio and use RAG for LLM queries.")
 
 # Include routers
-app.include_router(audio_router, prefix="/api/audio", tags=["Audio Processing"])
+app.include_router(audio_router, prefix="/api/audio",
+                   tags=["Audio Processing"])
 
 
 @app.get("/")
