0429b7860cbbad55eadfa165b29b56981fca8318
Kotmin <70173732+Kotmin@users.noreply.github.com>
2025-03-22 17:27:10 +0100

    feat: add toastify as responsive message dislpay

--- rag-audio-frontend/src/main.tsx
+++ rag-audio-frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100">
       <App />
+      <ToastContainer position="bottom-right" />
     </div>
   </React.StrictMode>
 );
