8b309162e89bf59b60e38917e61466d06fba709e
Kotmin <70173732+Kotmin@users.noreply.github.com>
2025-03-24 18:32:49 +0100

    feat: remove file after upload

--- RAGdio-backend/app/api/audio.py
+++ RAGdio-backend/app/api/audio.py
@@ -15,13 +15,26 @@ async def upload_audio(file: UploadFile = File(...)):
         temp_dir = Path("temp")
         temp_dir.mkdir(parents=True, exist_ok=True)
 
+
+        # prevent collisions?
+#         import uuid
+        # file_path = temp_dir / f"{uuid.uuid4()}.wav"
         file_path = f"temp/{file.filename}"
         with open(file_path, "wb") as buffer:
             buffer.write(await file.read())
 
         # text = whisper_processor.transcribe(file_path)
         text = "yes yes"
-        return {"filename": file.filename, "transcription": text}
+
+        Path(file_path).unlink(missing_ok=True)
+                # Return structured response
+        # return SendFileResponse(
+        #     filename=file.filename,
+        #     transcription=text
+        # )
+
+        # return {"filename": file.filename, "transcription": text}
+        return {"filename": "file ", "transcription": text}
 
     except ValueError as ve:
         raise HTTPException(status_code=400, detail=str(ve))
