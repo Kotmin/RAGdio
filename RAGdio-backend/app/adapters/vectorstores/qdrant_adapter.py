2d80953469a8e048229afaa88a65e68da6dd07f6
Kotmin <70173732+Kotmin@users.noreply.github.com>
2025-04-03 00:48:17 +0200

    fix: rm permantent vectorstore recreation

--- RAGdio-backend/app/adapters/vectorstores/qdrant_adapter.py
+++ RAGdio-backend/app/adapters/vectorstores/qdrant_adapter.py
@@ -1,8 +1,11 @@
-# from langchain_community.vectorstores import Qdrant
-from langchain_qdrant import Qdrant
+
 from langchain.embeddings.base import Embeddings
+
+from langchain_qdrant import Qdrant
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance
+from qdrant_client.http.exceptions import UnexpectedResponse
+
 from langchain.schema import Document
 from .base import VectorStoreAdapter
 from typing import List
@@ -10,36 +13,6 @@ import os
 
 import logging
 from app.core.config import Config
-
-# class QdrantVectorStoreAdapter(VectorStoreAdapter):
-#     def __init__(self, embedding_model: Embeddings, collection_name="rag_audio_collection"):
-#         self.collection_name = collection_name
-#         self.client = QdrantClient(
-#             host=os.getenv("QDRANT_HOST", "localhost"),
-#             port=int(os.getenv("QDRANT_PORT", 6333)),
-#         )
-
-#         self.embedding_model = embedding_model
-
-#         existing_collections = self.client.get_collections().collections
-#         if self.collection_name not in [c.name for c in existing_collections]:
-#             self.client.recreate_collection(
-#                 collection_name=self.collection_name,
-#                 vectors_config=VectorParams(size=768, distance=Distance.COSINE),
-#             )
-
-#         self.db = Qdrant(
-#             client=self.client,
-#             collection_name=self.collection_name,
-#             embeddings=self.embedding_model,
-#         )
-
-#     def add_documents(self, docs: List[Document]) -> None:
-#         self.db.add_documents(docs)
-
-#     def as_retriever(self):
-#         return self.db.as_retriever(search_type="mmr", search_kwargs={"k": 3})
-
 
 
 class QdrantVectorStoreAdapter(VectorStoreAdapter):
@@ -84,9 +57,15 @@ class QdrantVectorStoreAdapter(VectorStoreAdapter):
                         collection_name=self.collection_name,
                         vectors_config=VectorParams(size=embedding_dim, distance=Distance.COSINE),
                     )
-        except Exception:
-            if Config.DEBUG:
-                logging.info(f"🆕 Creating Qdrant collection '{self.collection_name}' with dim {embedding_dim}")
+        # except Exception:
+        #     if Config.DEBUG:
+        #         logging.info(f"🆕 Creating Qdrant collection '{self.collection_name}' with dim {embedding_dim}")
+        #     self.client.recreate_collection(
+        #         collection_name=self.collection_name,
+        #         vectors_config=VectorParams(size=embedding_dim, distance=Distance.COSINE),
+        #     )
+        except UnexpectedResponse as e:
+            logging.warning(f"Collection '{self.collection_name}' does not exist, creating it.")
             self.client.recreate_collection(
                 collection_name=self.collection_name,
                 vectors_config=VectorParams(size=embedding_dim, distance=Distance.COSINE),
