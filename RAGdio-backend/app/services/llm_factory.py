4d65278337cd1870f64328af9f61fde3daa0af14
Kotmin <70173732+Kotmin@users.noreply.github.com>
2025-04-11 01:50:07 +0200

    feat: add deepseek and ollama to llm_factory

--- RAGdio-backend/app/services/llm_factory.py
+++ RAGdio-backend/app/services/llm_factory.py
@@ -4,6 +4,9 @@ from app.core.config import Config
 from app.adapters.llms.base import ChatModelAdapter
 
 from app.adapters.llms.openai_adapter import OpenAIChatAdapter
+from app.adapters.llms.ollama_adapter import OllamaChatAdapter
+from app.adapters.llms.deepseek_adapter import DeepSeekChatAdapter
+
 
 
 # from app.adapters.llms.local_adapter import LocalChatAdapter  # optional
@@ -13,7 +16,11 @@ def get_chat_model() -> ChatModelAdapter:
 
     if model_name == "openai":
         return OpenAIChatAdapter()
-    # elif model_name == "local":
+    if model_name == "ollama":
+        return OllamaChatAdapter()
+    if model_name == "deepseek":
+        return DeepSeekChatAdapter()
+    # if model_name == "local":
     #     return LocalChatAdapter()
     else:
         raise ValueError(f"Unsupported LLM backend: {model_name}")
